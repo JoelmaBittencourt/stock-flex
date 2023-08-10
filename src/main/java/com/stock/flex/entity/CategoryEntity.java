dd0f60cc4acd781caa8311b60dc7000a6c63b910
joelma <joelmadev@gmail.com>
2023-08-10 08:47:30 -0300

    Adição de entidades

--- src/main/java/com/stock/flex/entity/CategoryEntity.java
+++ src/main/java/com/stock/flex/entity/CategoryEntity.java
@@ -1,7 +1,36 @@
 package com.stock.flex.entity;
 
 
-public class CategoryEntity{
+import com.stock.flex.resource.request.CategoryRequest;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "category")
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String name;
+    private String description;
+    private List<String> products;
+
+    public CategoryEntity(CategoryRequest request) {
+        this.name = request.name();
+        this.description = request.description();
+        this.products = request.products();
+    }
+
 
 }
-
--- src/main/java/com/stock/flex/entity/StockEntity.java
+++ src/main/java/com/stock/flex/entity/StockEntity.java
@@ -1,6 +1,36 @@
 package com.stock.flex.entity;
 
+import com.stock.flex.resource.request.StockRequest;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "stock")
 public class StockEntity {
 
-}
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String name;
+    private String description;
+    private boolean ativo;
+    private List<String> category;
 
+    public StockEntity(StockRequest request) {
+        this.name = request.name();
+        this.description = request.description();
+        this.category = new ArrayList<String>();
+    }
+
+}
