b005aed676cf474ca7476198636173ff466110dd
joelma <joelmadev@gmail.com>
2023-08-11 13:01:39 -0300

    Atualizando crud de category

--- src/main/java/com/stock/flex/resource/CategoryResource.java
+++ src/main/java/com/stock/flex/resource/CategoryResource.java
@@ -7,7 +7,9 @@ import com.stock.flex.resource.response.CategoryResponse;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 import java.util.UUID;
@@ -20,23 +22,45 @@ public class CategoryResource {
     CategoryRepository repository;
 
     @PostMapping
-    public void create(@Valid @RequestBody CategoryRequest request){
-        repository.save(new CategoryEntity(request));
+    public ResponseEntity create(@Valid @RequestBody CategoryRequest request, UriComponentsBuilder uriBuilder) {
+        var cqategoryEntity = new CategoryEntity(request);
+        repository.save(cqategoryEntity);
+        var uri = uriBuilder.path("/category/{id}").buildAndExpand(cqategoryEntity.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new CategoryResponse(cqategoryEntity));
     }
 
     @GetMapping
-    public List<CategoryResponse> get() {
-        return repository.findAll().stream().map(CategoryResponse::new).toList();
+    public ResponseEntity<List<CategoryResponse>> get() {
+        var list = repository.findAll().stream().map(CategoryResponse::new).toList();
+        return ResponseEntity.ok(list);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable UUID id) {
+        var category = repository.getReferenceById(id);
+        return ResponseEntity.ok(new CategoryResponse(category));
+    }
+
     @PutMapping
     @Transactional
-    public void update(@RequestBody CategoryResponse response) {
+    public ResponseEntity<CategoryResponse> responseEntity(@RequestBody CategoryResponse response) {
         var product = repository.getReferenceById(response.id());
         product.updateInfo(response);
+        return ResponseEntity.ok(new CategoryResponse(product));
     }
+    @PutMapping("/{id}")
+    @Transactional
+    public ResponseEntity<CategoryResponse> updateCategory(@PathVariable UUID id, @RequestBody CategoryResponse response) {
+        var category = repository.getReferenceById(id);
+        category.updateInfo(response);
+        return ResponseEntity.ok(new CategoryResponse(category));
+    }
+
     @DeleteMapping("/{id}")
     @Transactional
-    public void delete(@PathVariable UUID id) {
+    public ResponseEntity delete(@PathVariable UUID id) {
         repository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
