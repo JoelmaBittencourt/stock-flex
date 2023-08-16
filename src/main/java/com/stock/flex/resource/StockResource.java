d66512045b1da6d61b19754b024094f9973c7820
joelma <devjoelma@gmail.com>
2023-08-16 18:08:51 -0300

    Atualização de estoque

--- src/main/java/com/stock/flex/resource/StockResource.java
+++ src/main/java/com/stock/flex/resource/StockResource.java
@@ -2,11 +2,14 @@ package com.stock.flex.resource;
 
 import com.stock.flex.entity.StockEntity;
 import com.stock.flex.repository.StockRepository;
+import com.stock.flex.resource.request.ProductRequest;
 import com.stock.flex.resource.request.StockRequest;
+import com.stock.flex.resource.response.ProductResponse;
 import com.stock.flex.resource.response.StockResponse;
 import jakarta.transaction.Transactional;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,25 +23,32 @@ public class StockResource {
     StockRepository repository;
 
     @PostMapping
-    public void create(@Valid @RequestBody StockRequest request){
-        repository.save(new StockEntity(request));
+    @Transactional
+    public ResponseEntity<StockResponse> create(@RequestBody StockRequest request) {
+        StockEntity savedStock = repository.save(new StockEntity(request));
+        StockResponse response = new StockResponse(savedStock);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     @GetMapping
-    public List<StockResponse> get() {
-        return repository.findAll().stream().map(StockResponse::new).toList();
+    public ResponseEntity<List<StockResponse>> get() {// se fosse todos os dados
+        var stock = repository.findAll().stream().map(StockResponse::new).toList();
+        return ResponseEntity.ok(stock);
     }
 
-    @PutMapping
+    @PutMapping("/{id}")
     @Transactional
-    public void update(@RequestBody StockResponse response) {
-        var product = repository.getReferenceById(response.id());
-        product.updateInfo(response);
+    public ResponseEntity<StockResponse> update(@PathVariable UUID id, @RequestBody StockRequest request) {
+        var stock = repository.getReferenceById(id);
+        stock.updateInfo(request);
+        return ResponseEntity.ok(new StockResponse(stock));
     }
+
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void delete(@PathVariable UUID id) {
+    public ResponseEntity delete(@PathVariable UUID id) {
         repository.deleteById(id);
+        return ResponseEntity.ok().build();
     }
 }
