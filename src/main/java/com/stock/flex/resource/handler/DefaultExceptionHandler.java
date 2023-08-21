236fe34b765925497652af9c8b2fa615af41e9c9
joelma <devjoelma@gmail.com>
2023-08-21 09:21:56 -0300

    Adição de opções para validação de erros

---
+++ src/main/java/com/stock/flex/resource/handler/DefaultExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.stock.flex.resource.handler;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class DefaultExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity handle404Error() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity handle400Error(MethodArgumentNotValidException ex) {
+        var errors = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(errors.stream().map(ValidationError::new).toList());
+    }
+
+
+}
--- src/main/java/com/stock/flex/resource/request/ProductRequest.java
+++ src/main/java/com/stock/flex/resource/request/ProductRequest.java
@@ -1,16 +1,21 @@
 package com.stock.flex.resource.request;
 
 import com.stock.flex.entity.ProductEntity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
-public record ProductRequest(String name,
-                             String description,
-                             double price,
-                             int quantity,
-                             int displayOrder,
-                             int starQuantity) {
+public record ProductRequest(
+        @NotBlank(message = "{nome.obrigatorio}")
+        String name,
+        String description,
+        double price,
+        int quantity,
+        int displayOrder,
+        @Pattern(regexp = "\\d{0,5}", message = "Deve conter um numero de 0 a 5")
+        int starQuantity) {
 
-    public ProductRequest(ProductEntity product){
-                          this(product.getName(), product.getDescription(), product.getPrice(), product.getQuantity(), product.getDisplayOrder(), product.getStarQuantity());
+    public ProductRequest(ProductEntity product) {
+        this(product.getName(), product.getDescription(), product.getPrice(), product.getQuantity(), product.getDisplayOrder(), product.getStarQuantity());
     }
 
 }
