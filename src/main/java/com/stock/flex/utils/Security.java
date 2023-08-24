34017fa4453dcde00b9dd7552d1e989ff825bd14
joelma <devjoelma@gmail.com>
2023-08-24 13:58:13 -0300

    Adição de dependencia e criação de SecurityConfig

---
+++ src/main/java/com/stock/flex/utils/Security.java
@@ -0,0 +1,12 @@
+package com.stock.flex.utils;
+
+public class Security {
+    public static final String[] PUBLIC_MATCHERS = {
+            "/"
+    };
+
+    public static final String[] PUBLIC_MATCHERS_POST = {
+            "/user",
+            "/login"
+    };
+}
