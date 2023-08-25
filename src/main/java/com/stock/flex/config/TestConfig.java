87e0f6dc3449530c386c850e6732bf0f9810f786
joelma <devjoelma@gmail.com>
2023-08-25 12:40:52 -0300

    Configuração de segurança

---
+++ src/main/java/com/stock/flex/config/TestConfig.java
@@ -0,0 +1,19 @@
+//package com.stock.flex.config;
+//
+//import com.stock.flex.security.DatabaseService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class TestConfig {
+//
+//	@Autowired
+//	private DatabaseService databaseService;
+//
+//	@Bean
+//	public void initializeDatabase() {
+//		databaseService.initializeDatabase();
+//	}
+//
+//}
---
+++ src/main/java/com/stock/flex/security/handler/StandardErrorDTO.java
@@ -0,0 +1,22 @@
+//package com.stock.flex.security;
+//
+//import jakarta.servlet.http.HttpServletRequest;
+//import lombok.Getter;
+//import org.springframework.http.HttpStatus;
+//
+//@Getter
+//public class StandardErrorDTO {
+//
+//	private Integer status;
+//	private String error;
+//	private String message;
+//	private String path;
+//
+//	public StandardErrorDTO(HttpStatus status, Throwable ex, HttpServletRequest request) {
+//		this.status = status.value();
+//		this.error = ex.getClass().getSimpleName();
+//		this.message = ex.getMessage();
+//		this.path = request.getRequestURI();
+//	}
+//
+//}
