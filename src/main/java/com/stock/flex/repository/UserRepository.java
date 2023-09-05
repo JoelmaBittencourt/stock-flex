5fb273c29fa1c12e3d5efba9aa360da6dec8bce5
joelma <devjoelma@gmail.com>
2023-09-05 13:19:04 -0300

    refactor user

--- src/main/java/com/stock/flex/repository/UserRepository.java
+++ src/main/java/com/stock/flex/repository/UserRepository.java
@@ -1,10 +1,10 @@
-package com.stock.flex.repository;
-
-import com.stock.flex.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface UserRepository extends JpaRepository<UserEntity, UUID> {
-
-}
+//package com.stock.flex.repository;
+//
+//import com.stock.flex.entity.UserEntity;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import java.util.UUID;
+//
+//public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+//
+//}
--- src/main/java/com/stock/flex/resource/UserResource.java
+++ src/main/java/com/stock/flex/resource/UserResource.java
@@ -1,58 +1,58 @@
-package com.stock.flex.resource;
-
-import com.stock.flex.entity.UserEntity;
-import com.stock.flex.repository.UserRepository;
-import com.stock.flex.resource.request.UserRequest;
-import com.stock.flex.resource.response.UserResponse;
-import jakarta.transaction.Transactional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.UUID;
-
-@RestController
-@RequestMapping("/user")
-
-public class UserResource {
-
-    @Autowired
-    UserRepository repository;
-
-    @PostMapping
-    @Transactional
-    public ResponseEntity<UserResponse> create(@RequestBody UserRequest request) {
-        UserEntity saveduser = repository.save(new UserEntity(request));
-        UserResponse response = new UserResponse(saveduser);
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
-    }
-
-    @GetMapping
-    public ResponseEntity<List<UserResponse>> get() {
-        var user = repository.findAll().stream().map(UserResponse::new).toList();
-        return ResponseEntity.ok(user);
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity getById(@PathVariable UUID id) {
-        var user = repository.getById(id);
-        return ResponseEntity.ok(new UserResponse(user));
-    }
-
-    @PutMapping("/{id}")
-    @Transactional
-    public ResponseEntity<UserRequest> update(@PathVariable UUID id, @RequestBody UserRequest request) {
-        var user = repository.getReferenceById(id);
-        user.updateInfo(request);
-        return ResponseEntity.ok(new UserRequest(user));
-    }
-
-    @DeleteMapping("/{id}")
-    @Transactional
-    public ResponseEntity delete(@PathVariable UUID id) {
-        repository.deleteById(id);
-        return ResponseEntity.ok().build();
-    }
-}
+//package com.stock.flex.resource;
+//
+//import com.stock.flex.entity.UserEntity;
+//import com.stock.flex.repository.UserRepository;
+//import com.stock.flex.resource.request.UserRequest;
+//import com.stock.flex.resource.response.UserResponse;
+//import jakarta.transaction.Transactional;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//import java.util.UUID;
+//
+//@RestController
+//@RequestMapping("/user")
+//
+//public class UserResource {
+//
+//    @Autowired
+//    UserRepository repository;
+//
+//    @PostMapping
+//    @Transactional
+//    public ResponseEntity<UserResponse> create(@RequestBody UserRequest request) {
+//        UserEntity saveduser = repository.save(new UserEntity(request));
+//        UserResponse response = new UserResponse(saveduser);
+//        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+//    }
+//
+//    @GetMapping
+//    public ResponseEntity<List<UserResponse>> get() {
+//        var user = repository.findAll().stream().map(UserResponse::new).toList();
+//        return ResponseEntity.ok(user);
+//    }
+//
+//    @GetMapping("/{id}")
+//    public ResponseEntity getById(@PathVariable UUID id) {
+//        var user = repository.getById(id);
+//        return ResponseEntity.ok(new UserResponse(user));
+//    }
+//
+//    @PutMapping("/{id}")
+//    @Transactional
+//    public ResponseEntity<UserRequest> update(@PathVariable UUID id, @RequestBody UserRequest request) {
+//        var user = repository.getReferenceById(id);
+//        user.updateInfo(request);
+//        return ResponseEntity.ok(new UserRequest(user));
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    @Transactional
+//    public ResponseEntity delete(@PathVariable UUID id) {
+//        repository.deleteById(id);
+//        return ResponseEntity.ok().build();
+//    }
+//}
