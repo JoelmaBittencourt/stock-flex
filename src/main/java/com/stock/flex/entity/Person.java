50f07b17997834ad6ef126b5a3b45615c157f248
joelma <devjoelma@gmail.com>
2023-09-08 10:16:36 -0300

    Inicialização do banco

--- src/main/java/com/stock/flex/entity/Person.java
+++ src/main/java/com/stock/flex/entity/Person.java
@@ -5,6 +5,7 @@ import com.stock.flex.resource.request.PersonResponse;
 import com.stock.flex.entity.enums.Role;
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,9 +14,11 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 @Entity
+@Getter
+@Setter
+@Table(name = "person")
 public class Person implements  UserDetails {
 
-	@Getter
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private UUID id;
@@ -70,37 +73,11 @@ public class Person implements  UserDetails {
 		this.setStringRoles(dto.getRoles());
 	}
 
-	public String getName() {
-		return name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
 
 	public Set<Role> getRoles() {
 		return roles.stream().map(r -> Role.fromId(r)).collect(Collectors.toSet());
 	}
 
-	public void setId(UUID id) {
-		this.id = id;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
 
 	public void setRoles(Set<Role> roles) {
 		if (roles == null || roles.isEmpty())
--- src/main/java/com/stock/flex/entity/UserEntity.java
+++ src/main/java/com/stock/flex/entity/UserEntity.java
@@ -1,48 +1,48 @@
-package com.stock.flex.entity;
-
-import com.stock.flex.resource.request.StockRequest;
-import com.stock.flex.resource.request.UserRequest;
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import java.util.List;
-import java.util.UUID;
-import java.util.stream.Collectors;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-
-@Entity
-@Table(name = "users")
-public class UserEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private UUID id;
-
-    private String username;
-    private String password;
-
-    @OneToMany
-    @JoinColumn(name = "users_id")
-    private List<StockEntity> stocks;
-
-    public UserEntity(UserRequest request) {
-        this.username = request.username();
-        this.password = request.password();
-    }
-
-    public void updateInfo(UserRequest request) {
-        if (request.username() != null) {
-            this.username = request.username();
-        }
-        if (request.password() != null) {
-            this.password = request.password();
-        }
-    }
-}
+//package com.stock.flex.entity;
+//
+//import com.stock.flex.resource.request.StockRequest;
+//import com.stock.flex.resource.request.UserRequest;
+//import jakarta.persistence.*;
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//
+//import java.util.List;
+//import java.util.UUID;
+//import java.util.stream.Collectors;
+//
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
+//
+//@Entity
+//@Table(name = "users")
+//public class UserEntity {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private UUID id;
+//
+//    private String username;
+//    private String password;
+//
+//    @OneToMany
+//    @JoinColumn(name = "users_id")
+//    private List<StockEntity> stocks;
+//
+//    public UserEntity(UserRequest request) {
+//        this.username = request.username();
+//        this.password = request.password();
+//    }
+//
+//    public void updateInfo(UserRequest request) {
+//        if (request.username() != null) {
+//            this.username = request.username();
+//        }
+//        if (request.password() != null) {
+//            this.password = request.password();
+//        }
+//    }
+//}
