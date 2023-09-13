006dd7edf9489e245af5dd92f8238701c3aa8e0b
joelma <devjoelma@gmail.com>
2023-09-13 09:16:35 -0300

    Refactor de person para user

--- src/main/java/com/stock/flex/resource/AuthResource.java
+++ src/main/java/com/stock/flex/resource/AuthResource.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-public class AuthController {
+public class AuthResource {
 	
 	@Autowired
 	private AuthService authService;
